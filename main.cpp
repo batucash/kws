099c340e08da9d4654a6a65b1e218bd06986f555
Batuhan Gundogdu-MacPro <bgundogdu16@gmail.com>
2016-07-08 00:38:34 +0300

    Sets the mainframe of the distance learning

--- main.cpp
+++ main.cpp
@@ -3,15 +3,18 @@
 #include <armadillo>
 using namespace std;
 using namespace arma;
-const int PHONESIZE = 43;
+const int NUMPHONES = 43;
+
 
 int main(int argc, char *argv[]){
     
-    mat A = randu<mat>(5,5);
-    A.print();
+    //  1.  Read frames and labels
+    //  2.  Segment into phoneme classes keeps just the indices I think
+    //  3.  Create the training set friends and foes
+    //  4.  Initialize weights
+    //  5.  Learn Parameters
     
-    
-    
+
     
     return 0;
     
--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ output=out.exe
 ####################Functions to Be Used ###################
 
 ###########################################################
-g++ main.cpp -o $outfile/$output -O2 -I ~/Desktop/Research/software/armadillo-4.600.4/include -DARMA_DONT_USE_WRAPPER -lblas -llapack
+g++ main.cpp -o $outfile/$output -O2 -I ~/Desktop/Research/software/armadillo-7.200.2/include -DARMA_DONT_USE_WRAPPER -lblas -llapack
 #cd $outfile&
 ./$outfile/$output
 
